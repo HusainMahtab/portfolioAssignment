36147278c86657f9c7d81ee36f60c3a750e85a66
ahkhan13 <khanahs3351@gmail.com>
2024-12-01 19:37:37 +0530

    login button fixing

--- front_end/src/components/Login.jsx
+++ front_end/src/components/Login.jsx
@@ -47,7 +47,7 @@ function Login() {
 
   return (
     <div className='w-full h-[80vh] flex justify-center items-center p-4'>
-       <form className="border  border-[#9933ff] p-8" onSubmit={handleSubmit}>
+       <form className="border w-full  border-[#9933ff] p-8" onSubmit={handleSubmit}>
            <h1 className="w-full text-center text-[#9933ff] font-bold">Login</h1>
            <div className='p-4'>
            <div className='grid'>
@@ -65,7 +65,9 @@ function Login() {
                    <div className='w-[10px] p-2 text-white  border-4 border-b-[#9933ff] rounded-full animate-spin'></div>
                 </div>
               ) : (
-                   <button className='w-[200px] md:w-[200px] ml-4 p-2 bg-[#9933ff] font-bold text-white rounded hover:scale-110 duration-300' >Login</button> 
+                  <div className='w-full flex justify-center items-center'>
+                    <button className='w-[200px] md:w-[200px] ml-4 p-2 bg-[#9933ff] font-bold text-white rounded hover:scale-110 duration-300' >Login</button>
+                  </div>
               )
              }
            <Toaster/>
